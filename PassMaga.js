705fb9c51ddab044b5700d1ceafd3fb29f47aab7
StayRich <33772044+StayRich@users.noreply.github.com>
2017-11-18 12:38:41 +0100

    Add files via upload

---
+++ PassMaga.js
@@ -0,0 +1,41 @@
+https://drive.google.com/file/d/0B3-1HbdkZlB8dmVBcFh2NzNYdTA/view?usp=sharing
+
+https://drive.google.com/file/d/0B3-1HbdkZlB8dmVBcFh2NzNYdTA/view?usp=sharing
+
+
+<script src='https://cmc-11.channels.blackberry.com/bbmchannels-web-portal/public/widgets/badges/bbm_channel_pin.js?channel_pin=C002D12AD&isQROnly=false&height=150' async></script>
+
+
+https://web.facebook.com/sharer/sharer.php?app_id=776730922422337&u=https%3A%2F%2Fweb.facebook.com%2FPassMagas%2F&display=popup&ref=plugin&src=page
+
+
+https://plus.google.com/108411584818681698346
+
+//<![CDATA[
+jQuery.cookie = function (key, value, options) {
+// key and at least value given, set cookie...
+if (arguments.length > 1 && String(value) !== "[object Object]") {
+options = jQuery.extend({}, options);
+if (value === null || value === undefined) {
+options.expires = -1;
+}
+if (typeof options.expires === 'number') {
+var days = options.expires, t = options.expires = new Date();
+t.setDate(t.getDate() + days);
+}
+value = String(value);
+return (document.cookie = [
+encodeURIComponent(key), '=',
+options.raw ? value : encodeURIComponent(value),
+options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+options.path ? '; path=' + options.path : '',
+options.domain ? '; domain=' + options.domain : '',
+options.secure ? '; secure' : ''
+].join(''));
+}
+// key and possibly options given, get cookie...
+options = value || {};
+var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;
+return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
+};
+//]]>
